bb6225120826b8b95f086faf12d9b85ac6f0c04a
YakovDeLeon <40718860+YakovDeLeon@users.noreply.github.com>
2018-12-17 22:27:57 +0300

    Мелкие правки
    Добавлено сравнение параллельной и последовательной версии.
    Исправлены мелкие ошибки.

--- 1606-1/yakovlev_dl_task3.cpp
+++ 1606-1/yakovlev_dl_task3.cpp
@@ -1,480 +1,339 @@
-#include <mpi.h>
+#include "mpi.h"
 #include <iostream>
-#include <memory>
-#include <ctime>
+#include <time.h>
+#include <chrono>
 #include <limits>
-#include <cassert>
+#include <string>
 
-#pragma warning (disable: 4703)
-
-void FillArray(double * pd2dArray, size_t unSizeArr, double dBegin, double dEnd);
-void ShowArray(double * pdArray, size_t unSizeArr, int ProcRank);
-void* PointerOffset(MPI_Datatype type, void* buf, unsigned int nOffset);
-int OlderBit(int nNum);
-int NumberOlderBit(int nNum);
-template <typename Type>
-int Shell_Reduce(Type *sendbuf, int *sendcount, Type *recvbuf, int sizearr, MPI_Datatype type, int root, MPI_Comm comm);
-
-
-int main(int argc, char** argv)
+template<typename T>
+void ShellSort(T* array, int n)
 {
-	int nSizeArr;
-	int ProcRank, ProcNum;
-
-	MPI_Init(&argc, &argv);
-	MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
-	MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
-	if (ProcRank == 0)
-	{
-		if (argc == 4)
-		{
-			nSizeArr = atoi(argv[1]);
-		}
-		else
-		{
-			nSizeArr = 1000;
-		}
-	}
-
-	MPI_Bcast(&nSizeArr, 1, MPI_INT, 0, MPI_COMM_WORLD);
-
-	double * pdArr, *pdRecvArr;
-	double * pdRes = new double[nSizeArr] {0};
-
-	/*if (nSizeArr >= ProcNum || (ProcRank == 0 && nCols < ProcNum))
-		pdSumArray = new double[nCols] {0};
-	*/
-
-	double dStartTime;
-	if (ProcRank == 0)
-	{
-		pdArr = new double[nSizeArr];
-		if (argc == 5)
-			FillArray(pdArr, nSizeArr, atof(argv[3]), atof(argv[4]));
-		else
-			FillArray(pdArr, nSizeArr, 0.f, 10.f);
-		ShowArray(pdArr, nSizeArr, 0);
-		dStartTime = MPI_Wtime();
-		std::cout << "Computer has been beginning a solving!" << std::endl;
-	}
-	else
-	{
-		pdArr = nullptr;
-	}
-
-	/*if (nCols < ProcNum && ProcRank == 0)
-	{
-		int nCount = 0;
-		for (int i = 0; i < nCols * nRows; i++)
-		{
-			if (i != 0 && i % nRows == 0)
-				nCount++;
-			pdSumArray[nCount] += pdMatrix[i];
-		}
-
-	}*/
-
-	
-	int * pnArrCounts = new int[ProcNum];
-	int * pnArrDispls = new int[ProcNum];
-	if (ProcRank == 0)
-	{
-		int nSendSum = 0;
-		int nRecvSum = 0;
-		int nRemSizePerProc = nSizeArr % ProcNum;
-		for (size_t i = 0; i < ProcNum; i++)
-		{
-			pnArrCounts[i] = nSizeArr / ProcNum ;
-			if (nRemSizePerProc > 0)
+	T tmp;
+	for (int step = n / 2; step > 0; step /= 2)
+		for (int i = step; i < n; i++)
+			for (int j = i - step; (j >= 0) && (array[j] > array[j + step]); j -= step)
 			{
-				pnArrCounts[i]++;
-				nRemSizePerProc--;
+				tmp = array[j];
+				array[j] = array[j + step];
+				array[j + step] = tmp;
 			}
-			pnArrDispls[i] = nRecvSum;
-			nRecvSum += pnArrCounts[i];
-		}
-	}
-	MPI_Bcast(pnArrCounts, ProcNum, MPI_INT, 0, MPI_COMM_WORLD);
-	MPI_Bcast(pnArrDispls, ProcNum, MPI_INT, 0, MPI_COMM_WORLD);
-
-	//pdChunksOfSumArray = new double[pnColCounts[ProcRank]]{ 0 };
-	pdRecvArr = new double[pnArrCounts[ProcRank]];
-
-	MPI_Scatterv(pdArr, pnArrCounts, pnArrDispls, MPI_DOUBLE, pdRecvArr, pnArrCounts[ProcRank], MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
-	/*int nCount = 0;
-	for (int i = 0; i < pnColCounts[ProcRank] * nRows; i++)
-	{
-		if (i != 0 && i % nRows == 0)
-			nCount++;
-		pdChunksOfSumArray[nCount] += pdRecvMatrix[i];
-	}
-
-	MPI_Gatherv(pdChunksOfSumArray, nCount + 1, MPI_DOUBLE, pdSumArray, pnColCounts, pnColDispls, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-	*/
-	Shell_Reduce(pdRecvArr, pnArrCounts, pdRes, nSizeArr, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-	delete[] pnArrCounts, pnArrDispls, pdRecvArr;
-	
-	if (ProcRank == 0)
-	{
-		double dTimeDiff = MPI_Wtime() - dStartTime;
-		std::cout << "Time is " << dTimeDiff << " sec" << std::endl;
-	}
-
-	MPI_Finalize();
-	if (ProcRank == 0)
-	{
-		ShowArray(pdRes, nSizeArr, 0);
-		std::cout << std::endl;
-	}
-	delete[] pdArr, pdRes;
-	return 0;
 }
 
-void FillArray(double * pd2dArray, size_t unSizeArr, double dBegin, double dEnd)
+template<typename T>
+void FillingArray(T array[], int n, T min, T max) 
 {
 	srand(time(nullptr));
-	for (size_t i = 0; i < unSizeArr; ++i)
+	for (int i = 0; i < n; i++)
 	{
-		pd2dArray[i] = rand() % (int)(dEnd - dBegin) + dBegin + double(rand()) / 1000000.f;
+		T ri = (double)rand() / RAND_MAX;
+		array[i] = min + (max - min)*ri;
+	}
+	std::cout << std::endl;
+}
+
+
+template <typename T>
+void Comparator(T* arr, size_t size)
+{
+	for (size_t i = 1; i < size; i++)
+	{
+		if (arr[i] < arr[i - 1])
+			std::swap(arr[i], arr[i - 1]);
 	}
 }
 
-void ShowArray(double * pdArray, size_t unSizeArr, int ProcRank)
-{
-	for (size_t i = 0; i < unSizeArr; ++i)
-	{
-		std::cout << "ProcRank " << ProcRank << " has a folowing arr[" <<  i << "] = " << pdArray[i] << std::endl;
-	}
-}
-
-
-template<typename Type>
-Type* RealocArray(Type* pArr, size_t nOldSize, size_t nNewSize)
-{
-	assert(nOldSize <= nNewSize);
-	Type* pNewArr = new Type[nNewSize]{ 0 };
-	for (size_t i = 0; i < nOldSize; i++)
-	{
-		pNewArr[i] = pArr[i];
-	}
-	delete[] pArr;
-	pArr = nullptr;
-	return pNewArr;
-}
-
-template <typename BaseType>
-void ShellsSort(BaseType *A, unsigned N)
-{
-	unsigned i, j, k;
-	BaseType t;
-	for (k = N / 2; k > 0; k /= 2)
-		for (i = k; i < N; i++)
-		{
-			t = A[i];
-			for (j = i; j >= k; j -= k)
-			{
-				if (t < A[j - k])
-					A[j] = A[j - k];
-				else
-					break;
-			}
-			A[j] = t;
-		}
-}
-
-/*template<typename T>
-bool compare(const T a, const T b)
-{
-return a < b;
-}
-template<>
-bool compare<const char*>(const char * a, const char * b)
-{
-if (strlen(a) == strlen(b))
-return strcmp(a, b) < 0;
-else
-return  strlen(a) < strlen(b);
-}
-
-template<>
-bool compare<char*>(char * a, char * b)
-{
-if (strlen(a) == strlen(b))
-return strcmp(a, b) < 0;
-else
-return  strlen(a) < strlen(b);
-}
-
-template<typename T>
-void copy(T * begin, T * end, T * dst)
-{
-for (size_t i = 0; begin + i != end; ++i)
-*(dst + i) = *(begin + i);
-}
-*/
-
-template<typename T>
-void Merge(T* mas1, T* mas2, T* tmp, size_t size1, size_t size2)
+template <typename T>
+void EvenSplitter(T* array, T* tmp1, T* tmp2, size_t size1, size_t size2)
 {
 	int a = 0;
 	int b = 0;
 	int i = 0;
-	while ((a != size1) && (b != size2))
+	bool ChangeI = size1 % 2;
+
+	while ((a < size1) && (b < size2))
 	{
-		if (mas1[a] <= mas2[b])
+
+		if (tmp1[a] <= tmp2[b])
 		{
-			tmp[i] = mas1[a];
-			a++;
+			array[i] = tmp1[a];
+			a += 2;
 		}
 		else
 		{
-			tmp[i] = mas2[b];
-			b++;
+			array[i] = tmp2[b];
+			b += 2;
 		}
-		i++;
+		i += 2;
+		if (i > size1 && ChangeI)
+		{
+			i -= 1;
+			ChangeI = false;
+		}
 	}
+
 	if (a == size1)
-	{
-		int j = b;
-		for (; j<size2; j++, i++)
-			tmp[i] = mas2[j];
-	}
+		for (int j = b; j < size2; j += 2, i += 2)
+		{
+			if (i > size1 && ChangeI)
+			{
+				i -= 1;
+				ChangeI = false;
+			}
+			array[i] = tmp2[j];
+		}
 	else
-	{
-		int j = a;
-		for (; j<size1; j++, i++)
-			tmp[i] = mas1[j];
-	}
+		for (int j = a; j < size1; j += 2, i += 2)
+		{
+			if (i > size1 && ChangeI)
+			{
+				i -= 1;
+				ChangeI = false;
+			}
+			array[i] = tmp1[j];
+		}
 }
 
+template <typename T>
+void OddSplitter(T* array, T* tmp1, T* tmp2, int size1, int size2)
+{
+	int a = 1;
+	int b = 1;
+	int i = 1;
+	bool ChangeI = size1 % 2;
+
+	while ((a < size1) && (b < size2))
+	{
+
+		if (tmp1[a] <= tmp2[b])
+		{
+			array[i] = tmp1[a];
+			a += 2;
+		}
+		else
+		{
+			array[i] = tmp2[b];
+			b += 2;
+		}
+		i += 2;
+		if (i >= size1 && ChangeI)
+		{
+			i += 1;
+			ChangeI = false;
+		}
+	}
+
+	if (a == size1)
+		for (int j = b; j < size2; j += 2, i += 2)
+		{
+			if (i >= size1 && ChangeI)
+			{
+				i += 1;
+				ChangeI = false;
+			}
+			array[i] = tmp2[j];
+		}
+	else
+		for (int j = a; j < size1; j += 2, i += 2)
+		{
+			if (i >= size1 && ChangeI)
+			{
+				i += 1;
+				ChangeI = false;
+			}
+			array[i] = tmp1[j];
+		}
+}
 
 template<typename T>
-void MergeArrays(T* mas1, T* mas2, T* tmp, size_t size1, size_t size2)
+void ShowArray(T * pdArray, size_t unSizeArr, int ProcRank)
 {
-	int nMedIndex = BinSearch(mas2, 0, size2, (mas1[size1 / 2]));
-	//std::cout << "Medium index = " << nMedIndex << std::endl;
-	int nTmp1Size = nMedIndex + size1 / 2;
-	int nTmp2Size = size1 + size2 - nTmp1Size;
-	T* tmp1 = new T[nTmp1Size];
-	T* tmp2 = new T[nTmp2Size];
-	Merge(mas1, mas2, tmp1, size1 / 2, nMedIndex);
-	Merge(mas1 + size1 / 2, mas2 + nMedIndex , tmp2, size1 - size1 / 2, size2 - (nMedIndex));
-	memcpy(tmp, tmp1, nTmp1Size * sizeof(T));
-	memcpy(tmp + nTmp1Size, tmp2, nTmp2Size * sizeof(T));
-	delete[] tmp1, tmp2;
-}
-
-template<typename T>
-int BinSearch(T *mas, int l, int r, T x)
-{
-	if (l == r)
-		return l;
-	if (l + 1 == r)
-		if (x < mas[l])
-			return l;
-		else
-			return r;
-	int m = (l + r) / 2;
-	if (x < mas[m])
-		r = m;
-	else
-		if (x > mas[m])
-			l = m;
-		else
-			return m;
-	return BinSearch(mas, l, r, x);
-}
-
-
-size_t TypeSize(MPI_Datatype type)
-{
-	size_t result;
-	switch (type)
+	for (size_t i = 0; i < unSizeArr; ++i)
 	{
-	default:
-		std::cout << "Incorrect datatype" << std::endl;
-		exit(1);
-	case MPI_CHAR:
-		result = sizeof(char);
-		break;
-	case MPI_UNSIGNED_CHAR:
-		result = sizeof(unsigned char);
-		break;
-	case MPI_SHORT:
-		result = sizeof(short);
-		break;
-	case MPI_UNSIGNED_SHORT:
-		result = sizeof(unsigned short);
-		break;
-	case MPI_INT:
-		result = sizeof(int);
-		break;
-	case MPI_UNSIGNED:
-		result = sizeof(unsigned int);
-		break;
-	case MPI_LONG:
-		result = sizeof(long);
-		break;
-	case MPI_UNSIGNED_LONG:
-		result = sizeof(unsigned long);
-		break;
-	case MPI_LONG_LONG_INT:
-		result = sizeof(long long);
-		break;
-	case MPI_FLOAT:
-		result = sizeof(float);
-		break;
-	case MPI_DOUBLE:
-		result = sizeof(double);
-		break;
-	case MPI_LONG_DOUBLE:
-		result = sizeof(long double);
-		break;
+		std::cout << "ProcRank " << ProcRank << " has a folowing arr[" << i << "] = " << pdArray[i] << std::endl;
 	}
-	return result;
-}
-void* PointerOffset(MPI_Datatype type, void* buf, unsigned int nOffset)
-{
-	void* buffer;
-	if (!buf)
-	{
-		std::cout << "Pointer must not be nullptr!" << std::endl;
-		exit(1);
-	}
-	// проверить константность? 
-	switch (type)
-	{
-	default:
-		std::cout << "Incorrect datatype" << std::endl;
-		exit(1);
-	case MPI_CHAR:
-		buffer = reinterpret_cast<char*>(buf) + nOffset;
-		break;
-	case MPI_UNSIGNED_CHAR:
-		buffer = reinterpret_cast<unsigned char*>(buf) + nOffset;
-		break;
-	case MPI_SHORT:
-		buffer = reinterpret_cast<short*>(buf) + nOffset;
-		break;
-	case MPI_UNSIGNED_SHORT:
-		buffer = reinterpret_cast<unsigned short*>(buf) + nOffset;
-		break;
-	case MPI_INT:
-		buffer = reinterpret_cast<int*>(buf) + nOffset;
-		break;
-	case MPI_UNSIGNED:
-		buffer = reinterpret_cast<unsigned int*>(buf) + nOffset;
-		break;
-	case MPI_LONG:
-		buffer = reinterpret_cast<long*>(buf) + nOffset;
-		break;
-	case MPI_UNSIGNED_LONG:
-		buffer = reinterpret_cast<unsigned long*>(buf) + nOffset;
-		break;
-	case MPI_LONG_LONG_INT:
-		buffer = reinterpret_cast<long long int*>(buf) + nOffset;
-		break;
-	case MPI_FLOAT:
-		buffer = reinterpret_cast<float*>(buf) + nOffset;
-		break;
-	case MPI_DOUBLE:
-		buffer = reinterpret_cast<double*>(buf) + nOffset;
-		break;
-	case MPI_LONG_DOUBLE:
-		buffer = reinterpret_cast<long double*>(buf) + nOffset;
-		break;
-	}
-	return buffer;
 }
 
-int OlderBit(int nNum)
+
+template <typename T>
+void BatcherMerge(T* arr1, T* arr2, T* res, size_t size1, size_t size2)
 {
-	int nTmp = 1 << 30;
-	while (nNum < nTmp)
-	{
-		nTmp >>= 1;
-	}
-	return nTmp;
+	memcpy(res, arr1, size1 * sizeof(T));
+	memcpy(res + size1, arr2, size2 * sizeof(T));
+	EvenSplitter(res, arr1, arr2, size1, size2);
+	OddSplitter(res, arr1, arr2, size1, size2);
+	Comparator(res, size1 + size2);
 }
 
-int NumberOlderBit(int nNum)
+template <typename T>
+void reallocate(T* &arr, size_t oldsize, size_t newsize)
 {
-	int nTmp = 1 << 30;
-	int nCount = 31;
-	while (nNum < nTmp)
-	{
-		nTmp >>= 1;
-		nCount--;
-	}
-	return nCount;
+	T* newarr = new T[newsize]{ 0 };
+	memcpy(newarr, arr, oldsize * sizeof(T));
+	delete[] arr;
+	arr = newarr;
 }
 
-template <typename Type>
-int Shell_Reduce(Type *sendbuf, int *sendcount, Type *recvbuf, int sizearr, MPI_Datatype type, int root, MPI_Comm comm)
+template <typename T>
+int TREE_Betcher(T *sendbuf, T *recvbuf, int* counts, int sizearr, MPI_Datatype type, int root, MPI_Comm comm)
 {
 	MPI_Status status;
 	int ProcRank, ProcNum;
 	MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
 	MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
-	int nRankSender{ 0 };
-	Type * pvResBuf = new Type[sizearr];
-	//recvbuf = malloc(sendcount[ProcRank] * TypeSize(type));
-	int nFinish = std::log(ProcNum) / std::log(2);
-	if (ProcNum % 2 > 0)
-		nFinish++;
-	ShellsSort((double*)sendbuf, sendcount[ProcRank]);
-	ShowArray(sendbuf, sendcount[ProcRank], ProcRank);
-	int newProcRank = (ProcRank < root ? ProcRank + 1 : ProcRank);
-	int nStart = (ProcRank == root ? 0 : NumberOlderBit(newProcRank));
-	for (int i = nFinish; i >= nStart; i--)
+	int RankRecv, RankSend;
+	T * ResBuf = new T[sizearr];
+	int RecvArrSize = 0;
+	int SendArrSize = counts[ProcRank];
+	int newProcRank = (ProcRank - root + ProcNum) % ProcNum;
+	int mask = 1;
+	while (mask < ProcNum)
 	{
-		if (ProcRank == root)
+		if ((newProcRank & mask) == 0)
 		{
-			nRankSender = std::pow(2, i);
-			nRankSender <= root ? nRankSender-- : nRankSender;
+			RankSend = newProcRank | mask;
+			if (RankSend < ProcNum)
+			{
+				RankSend = (RankSend + root) % ProcNum;
+				MPI_Recv(&RecvArrSize, 1, MPI_INT, RankSend, 0, comm, &status);
+				MPI_Recv(recvbuf, RecvArrSize, type, RankSend, 0, comm, &status);
+				
+				BatcherMerge(sendbuf, recvbuf, ResBuf, SendArrSize, RecvArrSize);
+				reallocate(sendbuf, SendArrSize, SendArrSize + RecvArrSize);
+
+				SendArrSize += RecvArrSize;
+				memcpy(sendbuf, ResBuf, SendArrSize * sizeof(T));
+				
+			}
 		}
 		else
 		{
-			nRankSender = std::pow(2, i) + newProcRank;
-			if (nRankSender <= root)
-				nRankSender--;
+			RankRecv = newProcRank&(~mask);
+			RankRecv = (RankRecv + root) % ProcNum;
+			MPI_Send(&SendArrSize, 1, MPI_INT, RankRecv, 0, comm);
+			MPI_Send(sendbuf, SendArrSize, type, RankRecv, 0, comm);
+			break;
 		}
-		if (nRankSender < ProcNum)
-		{
-			sendbuf = RealocArray(sendbuf, sendcount[ProcRank], sendcount[ProcRank] + sendcount[nRankSender]);
-			sendcount[ProcRank] += sendcount[nRankSender];
-			std::cout << ProcRank << " recv from " << nRankSender << " array with size " << sendcount[ProcRank] <<std::endl;
-			MPI_Recv(recvbuf, sendcount[ProcRank], type, nRankSender, 0, comm, &status);
-			MergeArrays(sendbuf, recvbuf, pvResBuf, sendcount[ProcRank] - sendcount[nRankSender], sendcount[nRankSender]);
-			
-			//recvbuf = realloc(sendbuf, sendcount[ProcRank] * TypeSize(type));
-			memcpy(sendbuf, pvResBuf, (sendcount[ProcRank]) * TypeSize(type));
-			std::cout << "	ProcRank " << ProcRank << " has a following array:" << std::endl;
-			ShowArray(sendbuf, sendcount[ProcRank], ProcRank);
-			std::cout << std::endl;
-			memset(recvbuf, 0, sendcount[ProcRank] * TypeSize(type));
-			//Operation(type, op, sendbuf, recvbuf, sendbuf, count);
-		}
+		mask = mask << 1;
 	}
 	if (ProcRank != root)
 	{
-		int oldnum = OlderBit(newProcRank);
-		int nRankReciever = newProcRank & (~oldnum);
-		if (nRankReciever == 0)
-			nRankReciever = root;
-		else if (nRankReciever <= root)
-			nRankReciever--;
-		std::cout << ProcRank << " send to " << nRankReciever << " array with size " << sendcount[ProcRank] << std::endl;
-		MPI_Send(sendbuf, sendcount[ProcRank], type, nRankReciever, 0, comm);
-	}
-	if (ProcRank == root)
-	{
-		memcpy(recvbuf, sendbuf, sizearr*TypeSize(type));
+		delete[] sendbuf;
+		delete[] ResBuf;
 	}
 
-	//ShowArray((double*)recvbuf, sendcount[ProcRank]);
-	//free(pvResBuf);
-	delete[] pvResBuf;
+	if (ProcRank == root)
+	{
+		memcpy(recvbuf, ResBuf, sizearr * sizeof(T));
+		delete[] sendbuf, ResBuf;
+	}
+
+	return 0;
+}
+
+
+int main(int argc, char *argv[])
+{
+	using namespace std;
+	using namespace std::chrono;
+	using time = chrono::steady_clock::time_point;
+	int ProcNum, ProcRank;
+	double *array = nullptr; 
+	int size, root;
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
+	MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
+
+	if (argc == 5)
+		root = atoi(argv[4]);
+	else
+		root = 0;
+	if (ProcRank == root)
+	{
+		if (argc == 5)
+			size = atoi(argv[1]);
+		else size = 10;
+	}
+	MPI_Bcast(&size, 1, MPI_INT, root, MPI_COMM_WORLD);
+
+	high_resolution_clock::time_point startTime;
+	high_resolution_clock::time_point endTime;
+	
+	if (ProcRank == root)
+	{
+		array = new double[size];
+		FillingArray(array, size, atoi(argv[2]), atoi(argv[3]));
+		//ShowArray(array, size);
+		
+	}
+	
+	int * sendcounts = new int[ProcNum];
+	int * displs = new int[ProcNum];
+	if (ProcRank == root)
+	{
+		int nSendSum = 0;
+		int nRecvSum = 0;
+		int nRemSizePerProc = size % ProcNum;
+		for (size_t i = 0; i < ProcNum; i++)
+		{
+			sendcounts[i] = size / ProcNum;
+			if (nRemSizePerProc > 0)
+			{
+				sendcounts[i]++;
+				nRemSizePerProc--;
+			}
+			displs[i] = nRecvSum;
+			nRecvSum += sendcounts[i];
+		}
+		startTime = chrono::high_resolution_clock::now();
+	}
+
+	MPI_Bcast(sendcounts, ProcNum, MPI_INT, root, MPI_COMM_WORLD);
+	MPI_Bcast(displs, ProcNum, MPI_INT, root, MPI_COMM_WORLD);
+
+	double *recvbuf = new double[sendcounts[ProcRank]];
+
+	MPI_Scatterv(array, sendcounts, displs, MPI_DOUBLE, recvbuf, sendcounts[ProcRank], MPI_DOUBLE, root, MPI_COMM_WORLD);
+
+	ShellSort(recvbuf, sendcounts[ProcRank]);
+
+	double * res = new double[size];
+	TREE_Betcher(recvbuf, res, sendcounts, size, MPI_DOUBLE, root, MPI_COMM_WORLD);
+	
+	if (ProcRank == root)
+	{
+		//std::cout << "Result for parallel version: " << std::endl;
+		//ShowArray(res, size);
+		endTime = chrono::high_resolution_clock::now();
+		auto WorkTimeParallel = chrono::duration_cast<chrono::nanoseconds>(endTime - startTime).count();
+		cout << "Time for parallel version: " << WorkTimeParallel << "ns" << endl;
+		startTime = chrono::high_resolution_clock::now();
+		ShellSort(array, size);
+		//std::cout << "Result for linear version: " << std::endl;
+		//ShowArray(array, size);
+		endTime = chrono::high_resolution_clock::now();
+		auto WorkTimeSerial = chrono::duration_cast<chrono::nanoseconds>(endTime - startTime).count();
+		cout << "Time for linear version: " << WorkTimeSerial << "ns" << endl;
+		string compare = (WorkTimeParallel < WorkTimeSerial ? "faster" : "slower");
+		cout << "Parallel version " << compare << " linear version!" << std::endl;
+		bool flag;
+		for (int i = 0; i < size; i++)
+		{
+			flag = true;
+			if (res[i] != array[i])
+			{
+				std::cout << "The parallel and linear version not matching" << std::endl;
+				flag = false;
+				break;
+			}
+		}
+		
+		if (flag)
+		{
+			std::cout << "The parallel and linear version matching" << std::endl;
+		}
+	}
+	MPI_Finalize();
+	delete[] array, sendcounts, displs, recvbuf, res;
 	return 0;
 }
